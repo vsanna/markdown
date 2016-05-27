bbf124c87c965a869552329c3149d5fe88f1c762
honake <honake@honake.local>
2016-05-27 18:04:16 +0900

    opacity window textarea float left undone

--- js/another_editor.js
+++ js/another_editor.js
@@ -5,10 +5,11 @@
             return hljs.highlightAuto(code, [lang]).value;
         }
     });
-    new Vue({
+    var master = new Vue({
         el: '#master',
         data: {
-            showMenu: false,
+            input: '',
+            showMenu: false
         },
         methods: {
             navigationOpen: function() {
@@ -18,70 +19,35 @@
             navigationClose: function() {
                 this.showMenu = false;
                 console.log('close')
-
+            },
+            Edit: function() {
+                document.getElementById("switch_1").style.display = "block";
+                document.getElementById("switch_2").style.display = "none";
+            },
+            View: function() {
+                document.getElementById("switch_1").style.display = "none";
+                document.getElementById("switch_2").style.display = "block";
+            },
+            OpenWindow: function() {
+                var rand = Math.floor(Math.random() * 31); // Randomization
+                var x = window.screenX - 300 - rand;
+                var y = window.screenY + rand;
+                window.open('another.html', '', 'width=300, height=400, left=' + x + ', top=' + y);
+            },
+            CloseWindow: function() {
+                var confirmation = confirm('Are you sure ?')
+                if (confirmation) {
+                    window.close()
+                }
             }
-        }
-    });
-    new Vue({
-        el: '#editor',
-        data: {
-            input: ''
         },
         filters: {
             marked: marked
         }
     });
-    /*  new Vue({
-              el: '#navigation',
-              data: {
-                  showMenu: false,
-              },
-              methods: {
-                  navigationOpen: function() {
-                      this.showMenu = true;
-                      console.log('open')
-                  },
-                  navigationClose: function() {
-                      this.showMenu = false;
-                      console.log('close')
-
-                  }
-              }
-          }
-      );*/
 }).call(this);
 
-// Navigation System
-
-function Display(no) {
-
-    if (no == "no1") {
-
-        document.getElementById("switch_1").style.display = "block";
-        document.getElementById("switch_2").style.display = "none";
-
-    } else if (no == "no2") {
-
-        document.getElementById("switch_1").style.display = "none";
-        document.getElementById("switch_2").style.display = "block";
-
-    }
-
-}
-
-function OpenWindow() {
-    var rand = Math.floor(Math.random() * 31); // Randomization
-    var x = window.screenX - 300 - rand;
-    var y = window.screenY + rand;
-    window.open('another.html', '', 'width=300, height=400, left=' + x + ', top=' + y);
-}
-
-function CloseWindow() {
-    var confirmation = confirm('Are you sure ?')
-    if (confirmation) {
-        window.close()
-    }
-}
+/* Navigation System
 
 function OpacityWindow() {
     console.log('opacity')
@@ -99,4 +65,4 @@ function WrapWindow() {
 
 function UnwrapWindow() {
     window.resizeTo(memoryX, memoryY);
-}
+} */
--- js/editor.js
+++ js/editor.js
@@ -8,6 +8,7 @@
     var master = new Vue({
         el: '#master',
         data: {
+            input: '## Markdown Stickies\r- Edit here\r- Write your to-do list\r> With Markdown Stickies.  \r> Nice and cool, isn\'t it ?\r\r### Help\r- Check our website: http://\r- Markdown Reference: http://\r\r|app name|ver|by|\r|-|-|-|\r|Markdown Stickies|0.1.0|honake|\r',
             showMenu: false
         },
         methods: {
@@ -18,52 +19,35 @@
             navigationClose: function() {
                 this.showMenu = false;
                 console.log('close')
-
+            },
+            Edit: function() {
+                document.getElementById("switch_1").style.display = "block";
+                document.getElementById("switch_2").style.display = "none";
+            },
+            View: function() {
+                document.getElementById("switch_1").style.display = "none";
+                document.getElementById("switch_2").style.display = "block";
+            },
+            OpenWindow: function() {
+                var rand = Math.floor(Math.random() * 31); // Randomization
+                var x = window.screenX - 300 - rand;
+                var y = window.screenY + rand;
+                window.open('another.html', '', 'width=300, height=400, left=' + x + ', top=' + y);
+            },
+            CloseWindow: function() {
+                var confirmation = confirm('Are you sure ?')
+                if (confirmation) {
+                    window.close()
+                }
             }
-        }
-    });
-    var editor = new Vue({
-        el: '#editor',
-        data: {
-            input: '## Markdown Stickies\r- Edit here\r- Write your to-do list\r> With Markdown Stickies.  \r> Nice and cool, isn\'t it ?\r\r### Help\r- Check our website: http://\r- Markdown Reference: http://\r\r|app name|ver|by|\r|-|-|-|\r|Markdown Stickies|0.1.0|honake|\r'
         },
         filters: {
             marked: marked
         }
-    })
+    });
 }).call(this);
 
-// Navigation System
-
-function Display(no) {
-
-    if (no == "no1") {
-
-        document.getElementById("switch_1").style.display = "block";
-        document.getElementById("switch_2").style.display = "none";
-
-    } else if (no == "no2") {
-
-        document.getElementById("switch_1").style.display = "none";
-        document.getElementById("switch_2").style.display = "block";
-
-    }
-
-}
-
-function OpenWindow() {
-    var rand = Math.floor(Math.random() * 31); // Randomization
-    var x = window.screenX - 300 - rand;
-    var y = window.screenY + rand;
-    window.open('another.html', '', 'width=300, height=400, left=' + x + ', top=' + y);
-}
-
-function CloseWindow() {
-    var confirmation = confirm('Are you sure ?')
-    if (confirmation) {
-        window.close()
-    }
-}
+/* Navigation System
 
 function OpacityWindow() {
     console.log('opacity')
@@ -81,4 +65,4 @@ function WrapWindow() {
 
 function UnwrapWindow() {
     window.resizeTo(memoryX, memoryY);
-}
+} */
